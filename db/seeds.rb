942baf58f5977e7455a2702f7c22672a7dea1d3a
PaulooMartin <paulmartinblopez@gmail.com>
2023-12-26 10:17:49 +0800

    Add flight seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+def rand_pick(max)
+  Random.rand(max)
+end
 
 Airport.destroy_all
-
 airport_codes = %w[MNL CEB ILO DVO CRK PPS CGY BCD TAC KLO ZAM TAG GES MPH]
-airport_codes.each do |code|
-  Airport.create!(iata_code: code)
+airport_codes.each { |code| Airport.create!(iata_code: code)}
+
+Flight.destroy_all
+10.times do |_i|
+  flight_data = {
+    duration: rand_pick(86400),
+    departure_date: DateTime.current.change(day: rand_pick(30) + 1, hour: rand_pick(23))
+  }
+  new_flight = Flight.find_or_initialize_by(flight_data)
+  new_flight.departure_airport = Airport.find_by(iata_code: airport_codes.sample)
+  new_flight.arrival_airport = Airport.find_by(iata_code: airport_codes.sample)
+  new_flight.save!
 end

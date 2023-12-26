afff05ec33a59f13c0cc810f1797cde2fd5c5e48
PaulooMartin <paulmartinblopez@gmail.com>
2023-12-26 10:17:25 +0800

    Add forgotten option on association

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,8 +1,8 @@
 class Airport < ApplicationRecord
   validates_presence_of :iata_code
 
-  has_many :departing_flights, class_name: "Flight", inverse_of: :departure_airport,
+  has_many :departing_flights, class_name: "Flight", inverse_of: :departure_airport, dependent: :destroy,
                               foreign_key: "departing_airport_id"
-  has_many :arriving_flights, class_name: "Flight", inverse_of: :arrival_airport,
+  has_many :arriving_flights, class_name: "Flight", inverse_of: :arrival_airport, dependent: :destroy,
                               foreign_key: "arriving_airport_id"
 end

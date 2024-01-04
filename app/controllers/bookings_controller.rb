a6d0f466f38811a1739c2fb02b829a196cdd6ba0
PaulooMartin <paulmartinblopez@gmail.com>
2024-01-04 15:01:02 +0800

    Added create action and strong params to booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  before_action :require_valid_params, only: :new
+  before_action :require_valid_search_params, only: :new
 
   def new
     @flight = Flight.find(params[:flight_id])
@@ -7,15 +7,28 @@ class BookingsController < ApplicationController
     params[:passenger_count].to_i.times { |_i| @booking.passengers.new }
   end
 
-  def create; end
+  def create
+    @booking = Booking.new(booking_params)
+
+    # root_path is temp
+    if @booking.save
+      redirect_to root_path, notice: 'Good job'
+    else
+      redirect_to root_path, alert: 'Failed'
+    end
+  end
 
   private
 
-  def require_valid_params
-    redirect_to(root_path, alert: 'Passenger count or Flight is invalid.') unless valid_booking_params?
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: %i[name email])
   end
 
-  def valid_booking_params?
+  def require_valid_search_params
+    redirect_to(root_path, alert: 'Passenger count or Flight is invalid.') unless valid_search_params?
+  end
+
+  def valid_search_params?
     params[:passenger_count].to_i.between?(1, 4) && Flight.exists?(id: params[:flight_id])
   end
 end

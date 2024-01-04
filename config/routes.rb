7284e70edeb995f8ba171dc15225c3c96f9d7046
PaulooMartin <paulmartinblopez@gmail.com>
2024-01-04 10:24:10 +0800

    Add routes for Bookings

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root "flights#index"
+  root 'flights#index'
   resources :flights, only: [:index]
+  resources :bookings, only: %i[new create]
 end

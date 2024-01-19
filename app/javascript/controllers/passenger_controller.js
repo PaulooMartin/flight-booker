02437c85a940b942d04b4e1082fe925945aa80b5
PaulooMartin <paulmartinblopez@gmail.com>
2024-01-19 13:50:45 +0800

    Add stimulus with add&remove passenger fields

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -7,17 +7,36 @@ export default class extends Controller {
   }
 
   connect() {
-    this.passengerCountValue = this.passengerFieldTargets.length
-    console.log("Hello from passenger controller")
+    this.passengerCountValue = this.passengerFieldTargets.length;
   }
 
   addPassenger(){
-    console.log("Added new passenger")
+    const nextPassengerIndex = this.passengerCountValue
+    const nameLabelForAndInputId = `booking_passengers_attributes_${nextPassengerIndex}_name`
+    const nameInputName = `booking[passengers_attributes][${nextPassengerIndex}][name]`    
+    const emailLabelForAndInputId = `booking_passengers_attributes_${nextPassengerIndex}_email`
+    const emailInputName = `booking[passengers_attributes][${nextPassengerIndex}][email]`
+
+    let newFieldFragment = this.fieldTemplateTarget.content.cloneNode(true);
+    let nameFieldArea = newFieldFragment.children[0].children[0]
+    let emailFieldArea = newFieldFragment.children[0].children[1]
+
+    this.#setFieldAreaAttributes(nameFieldArea, nameLabelForAndInputId, nameInputName)    
+    this.#setFieldAreaAttributes(emailFieldArea, emailLabelForAndInputId, emailInputName)    
+
+    this.passengerCountValue++
+    this.passengerFieldsAreaTarget.appendChild(newFieldFragment)
   }
 
-  removePassenger(){
-    console.log("Removed this passenger")
+  removePassenger(event){
+    if (this.passengerFieldsAreaTarget.children.length > 1){
+      event.target.parentNode.remove();
+    }
   }
 
-
+  #setFieldAreaAttributes(fieldArea, labelForAndInputId, inputName){
+    fieldArea.children[0].setAttribute('for', labelForAndInputId)
+    fieldArea.children[1].setAttribute('id', labelForAndInputId)
+    fieldArea.children[1].setAttribute('name', inputName)
+  }
 }
